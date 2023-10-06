b480145b79b5b8139fa4a765a5727e11a3f9b275
EC2 Default User <ec2-user@ip-172-31-13-13.ap-northeast-1.compute.internal>
2023-10-06 18:16:47 +0000

    コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'homes#top'
+  get "home/about"=>"homes#about", as: "about"
+
+  get 'users/show'
+  get 'users/edit'
+  get 'users/index'
+  get 'items/new'
+  get 'items/show'
+  get 'items/index'
+  get 'items/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20231006165850_create_items.rb
@@ -0,0 +1,11 @@
+class CreateItems < ActiveRecord::Migration[6.1]
+  def change
+    create_table :items do |t|
+      t.integer :user_id
+      t.string :name, null: false, default: ""
+      t.text :introduction, null: false, default: ""
+      t.references :genre, foreign_key: true
+      t.timestamps
+    end
+  end
+end
